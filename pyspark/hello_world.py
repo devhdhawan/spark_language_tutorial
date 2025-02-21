b18fbcbc946e62da7d177361788920b0fa7eb902
devdhawan <himanshudimp05@gmail.com>
2025-02-21 11:04:29 +0530

    feature Modification

--- pyspark/hello_world.py
+++ pyspark/hello_world.py
@@ -2,10 +2,4 @@ print("Welcome to Pyspark")
 
 print("Changes releated to Pyspark")
 
-<<<<<<< HEAD
-print("Changes releated to Pyspark:VersionA")
-=======
-print("Changes releated to featureA")
 
-print("Changes related to featureB")
->>>>>>> origin/main
