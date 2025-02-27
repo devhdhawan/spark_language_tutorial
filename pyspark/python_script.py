b6bd1248a994a869e877e68cd05510122068100a
devdhawan <himanshudimp05@gmail.com>
2025-02-27 16:11:47 +0530

    Changed_log:Mod12

--- pyspark/python_script.py
+++ pyspark/python_script.py
@@ -3,6 +3,7 @@ print("Hello Everyone !! Welcome to GITHUB ACTIONS.")
 
 import pandas as pd
 import os
+import json
 
 repo_root_dir = os.getenv("GITHUB_WORKSPACE")
 print(f"REPO PATH:{repo_root_dir}")
@@ -10,7 +11,11 @@ print(f"REPO PATH:{repo_root_dir}")
 changelog_path=os.path.join(repo_root_dir,"cicd/change_log.csv")
 print(f"Change Log CSV FILE PATH:{changelog_path}")
 
-df=pd.read_csv(changelog_path,delimiter="\t")
+df=pd.read_csv(changelog_path,delimiter="\t",names=['change','name'])
 
+df=df[df['change']!='D']
+
+vars=json.loads(os.getenv('CONTEXT_VARS'))
+print(vars)
 
 print(df)
