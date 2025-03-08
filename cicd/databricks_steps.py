2df90e3e4f07c83fe2beeb51d9eed8f5ed36c755
devdhawan <himanshudimp05@gmail.com>
2025-03-08 17:33:43 +0530

    Pipeline_change:9

--- cicd/databricks_steps.py
+++ cicd/databricks_steps.py
@@ -37,7 +37,11 @@ print(df.name)
 
 
 #<--------- WORKSPACE CLIENT OBJECT TO ACCESS WORKSPACE RESOURCES --------->
+print(f"DATABRICKS HOST {host}")
+print(f"DATABRICKS TOKEN {token}")
 ws=WorkspaceClient(host=host,token=token)
+
+print(f"DATABRICKS HOST:{ws}")
 
 def create_workflow(ws,job_json):
     headers = {
@@ -63,7 +67,8 @@ def deploy_workflow(ws,df):
         #<--------- CHECK IF THE JOB JSON IS MODIFIED OR NEWLY ADDED --------->
         if file.split('.')[1]=='json':
             print(f"JSON FILE : {file}")
-            workflow_file_name,_=os.path.splitext(os.path.basename(file))
+
+            workflow_file_name=file
             workflow_file_path=os.path.join(git_dir,workflow_file_name)
 
             with open(workflow_file_path,'r') as file:
