7106cc932ab0caf4d1cce858ed3a5e9df4a5b92e
devdhawan <himanshudimp05@gmail.com>
2025-03-14 15:16:32 +0530

    Config:Deployment

---
+++ pyspark/config_template.py
@@ -0,0 +1,11 @@
+env='<<ENV>>'
+
+databricks_host='<<DATABRICKS_HOST>>'
+get_alias='<<GET_ALIAS>>'
+repo_id='<<REPO_ID>>'
+dev_env_name='<<dev_env_name>>'
+dev_resource_group='<<dev_resource_grp>>'
+dev_ser_conn_name='<<dev_ser_conn_name>>'
+
+
+
