754b7cb43844c5b6197d8e984d51ced496b7753c
devdhawan <himanshudimp05@gmail.com>
2025-03-30 00:01:24 +0530

    Github:itr8

--- cicd/json_validator.py
+++ cicd/json_validator.py
@@ -1,6 +1,7 @@
 import json 
 import os
 import pandas as pd 
+from jsonpath_ng import parse
 
 repo_dir_path=os.getenv('GITHUB_WORKSPACE')
 print(f"REPO DIRECTORY PATH {repo_dir_path}")
@@ -16,6 +17,7 @@ print(json_df)
 
 env_lst=["dev","prod"]
 
+
 # print(df)
 
 print("******** GETTING THE CONTEXT VARS ********")
@@ -23,3 +25,38 @@ print("******** GETTING THE CONTEXT VARS ********")
 vars=json.loads(os.getenv('CONTEXT_VARS'))
 
 print(vars)
+
+json_service_principal=parse("run_as.service_principal_name")
+
+def get_match_value(data,json_expr):
+    return [match.value for match in json_expr.find(data)]
+
+
+def validate_json(json_df):
+
+    for job_json_dict in json_df.to_dict('records'):
+
+        _,json_path=json_df.values()
+
+        json_dir,json_name=os.path.split(json_path)
+
+        json_name,_=os.path.splitext(json_name)
+        print(f"JSON NAME:{json_name}")
+
+        try:
+            with open(json_path,'r') as file:
+                data=json.loads(file)
+        except:
+            print("Invalid JSON")
+
+        SPN=get_match_value(data,json_service_principal)
+        print(f"SERVICE PRINCIPAL:{SPN}")
+        
+
+
+
+        
+            
+
+
+    
---
+++ cicd/test_script.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+import json 
+from jsonpath_ng import parse
+
