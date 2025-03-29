095a582d62a3646adc6d89212954526addbaa50b
devdhawan <himanshudimp05@gmail.com>
2025-03-29 19:42:27 +0530

    Github:itr5

--- cicd/json_validator.py
+++ cicd/json_validator.py
@@ -1,5 +1,17 @@
 import json 
 import os
+import pandas as pd 
+
+repo_dir_path=os.getenv('GITHUB_WORKSPACE')
+print(f"REPO DIRECTORY PATH {repo_dir_path}")
+
+change_log_path=os.path.join(repo_dir_path,'cicd/change_log.csv')
+print(f"CHANGE LOG CSV PATH {change_log_path}")
+
+df=pd.read_csv(change_log_path,delimiter="\t",names=['change','name'])
+df=df[df['change']!='D']
+
+print(df)
 
 print("******** GETTING THE CONTEXT VARS ********")
 
